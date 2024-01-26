9ec53852ae15bc9c0637b819a5929d7d8c327d77
Ilvian <d.ilvian1993@gmail.com>
2024-01-26 10:59:40 +0000

    taask 1

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
+/*eslint-disable*/
 const process = require('process');
 const request = require('request');
 const api = process.argv[2];
 
 request(api, function (error, response, body) {
   if (error) {
-    console.error('error:', error);
+    console.error('error:', error); // Print the error if one occurred
   }
-  const films = JSON.parse(response.body); 
+  const films = JSON.parse(response.body); // Print the response status code if a response was received
   const filtered = films.results.filter(film => {
     for (const character of film.characters) {
       if (character.includes('18')) {
